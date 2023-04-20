00cdeea4954f6f0c78c9b5fb3525196d6dffbfae
VorosDominik <medoxidhun@gmail.com>
2023-04-20 20:59:33 +0200

    majdnem kész

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { Rendezes } from "./rendezes.js"
 import { szures } from "./rendezes.js"
 const NEVINPUTELEM=$("#nev1")
 const FAJTAINPUTELEM=$("#fajta1")
+
 $(document).ready(function () {
   init();
   
@@ -13,6 +14,16 @@ $(document).ready(function () {
     articleElem.html(osszeAllit(szurtlista));
     
     })
+  
+  $(document).ready(function () {
+  init();
+
+  $('#ujElemBtn').on('click', function() {
+    showNewElemForm();
+  });
+  
+  // ...
+});
 });
 
 function init() {
@@ -22,12 +33,11 @@ function init() {
   $('.törlés').on('click', function() {
     removeItem($(this).closest('tr').index() - 1);
   });
-  $("#knev, #kfajta, #kkor").on("click", function () {
+  $("#knev, #kfajta, #kkor").on("click", function (event) { // hozzáadtam az event paramétert
     const id = $(event.target).attr("id");
     Rendezes(ADATLISTA, id.slice(1));
     
     init();
-   
   });
 }
 
@@ -39,14 +49,11 @@ function osszeAllit(lista) {
     <th id="kfajta">fajta</th>
     <th id="kkor">kor</th>
     <th></th>
-
-   
     </tr>`;
   for (let index = 0; index < lista.length; index++) {
     txt += `<tr>`;
     for (const key in lista[index]) {
       txt += `<td>${lista[index][key]}</td>`;
-    
     }
     txt += '<td><button class="törlés">❌</button></td>'
     txt += `</tr>`;
@@ -59,3 +66,57 @@ function removeItem(index) {
   ADATLISTA.splice(index, 1);
   init();
 }
+
+function showNewElemForm() {
+  const formElem = `
+    <form id="newElemForm">
+      <label for="nev">Név:</label>
+      <input type="text" id="nev" name="nev"><br><br>
+      <label for="fajta">Fajta:</label>
+      <input type="text" id="fajta" name="fajta"><br><br>
+      <label for="kor">Kor:</label>
+      <input type="number" id="kor" name="kor"><br><br>
+      <button type="submit">Hozzáadás</button>
+    </form>
+  `;
+  const articleElem = $("article");
+  articleElem.html(formElem);
+  
+  $("#newElemForm").on("submit", function(event) {
+    event.preventDefault();
+    
+    const nev = $("#nev").val();
+    const fajta = $("#fajta").val();
+    const kor = $("#kor").val();
+    
+    ADATLISTA.push({
+      nev: nev,
+      fajta: fajta,
+      kor: kor
+    });
+    
+    init();
+  });
+}
+
+function hideNewElemForm() {
+  const articleElem = $("article");
+  articleElem.html(osszeAllit(ADATLISTA));
+
+  $(".törlés").on("click", function () {
+    removeItem($(this).closest("tr").index() - 1);
+  });
+
+  $("#knev, #kfajta, #kkor").on("click", function (event) {
+    const id = $(event.target).attr("id");
+    Rendezes(ADATLISTA, id.slice(1));
+
+    init();
+  });
+
+  $("#ujElemBtn").on("click", function () {
+    showNewElemForm();
+  });
+}
+
+$('#ujElemBtn').on
--- public.js
+++ public.js
@@ -23,6 +23,25 @@ function init() {
     removeItem($(this).closest('tr').index() - 1);
   });
 
+  $(".megvizsgal").on("click", function (event) {
+    const index = $(event.target).data("index");
+    console.log("Megvizsgál", ADATLISTA[index]);
+    showModal(index);
+    const modalBody = $(".modal-body");
+    modalBody.append(`<a href="#" class="btn btn-success kosarba-tesz-modal" data-index="${index}">Kosárba tesz</a>`);
+    modalBody.append(`<a href="#" class="btn btn-secondary">Tovább böngészek</a>`);
+    $('.kosarba-tesz-modal').on('click', function() {
+      const index = $(this).data("index");
+      console.log("Kosárba tesz (modális)", ADATLISTA[index]);
+    });
+  });
+  
+
+  $(".kosarbaTesz").on("click", function (event) {
+    const index = $(event.target).data("index");
+    console.log("Kosárba tesz", ADATLISTA[index]);
+  });
+
   $("#knev, #kfajta, #kkor").on("click", function (event) {
     const id = $(event.target).attr("id");
     Rendezes(ADATLISTA, id.slice(1));
@@ -30,38 +49,69 @@ function init() {
   });
 }
 
+  
 function osszeAllit(lista) {
-    let txt = "";
-  
-    txt += `<table class="table table-striped">`;
-    txt += `<tr class="table-dark"><th id="knev">név</th>
-      <th id="kfajta">fajta</th>
-      <th id="kkor">kor</th>
-      <th></th>
-  
-     
-      </tr>`;
-    for (let index = 0; index < lista.length; index++) {
-      
-      for (const key in lista[index]) {
-        txt += `<div class="card" style="width:400px">
-        <img class="card-img-top" src="img_avatar1.png" alt="Card image">
-        <div class="card-body">
-          <h4 class="card-title">${lista[index].nev}</h4>
-          <p class="card-text">fajta : ${lista[index].fajta}</p>
-          <a href="#" class="btn btn-primary">See Profile</a>
+  let txt = "";
+
+  txt += `<div class="row row-cols-1 row-cols-md-3 g-4">`;
+  for (let index = 0; index < lista.length; index++) {
+    txt += `
+      <div class="col">
+        <div class="card h-100">
+          <img class="card-img-top" src="kepek/23927031092.jpg" alt="kutya kép">
+          <div class="card-body">
+            <h4 class="card-title">${lista[index].nev}</h4>
+            <p class="card-text">fajta: ${lista[index].fajta}</p>
+            <button type="button" class="btn btn-success megvizsgal" data-index="${index}">Megvizsgál</button>
+            <button type="button" class="btn btn-secondary kosarba-tesz" data-index="${index}">Kosárba tesz</button>
+          </div>
         </div>
       </div>`;
-      
-      }
-     
-     
-    }
-    txt += `</table>`;
-    return txt;
   }
 
+  txt += `</div>`;
+
+  return txt;
+}
 function removeItem(index) {
   ADATLISTA.splice(index, 1);
   init();
 }
+    function showModal(index) {
+      let card = $(`.card:eq(${index})`);
+      let modalContent = card.clone();
+      const modal = $("<div>").addClass("modal");
+      const closeButton = $("<button>").addClass("close-button").text("×");
+      const leftButton = $("<button>").addClass("modal-nav-button left-button").html("<");
+      const rightButton = $("<button>").addClass("modal-nav-button right-button").html(">");
+      const modalContainer = $("<div>").addClass("modal-container").append(closeButton, leftButton, modalContent, rightButton);
+      
+      modal.append(modalContainer);
+      $("body").append(modal);
+      
+      closeButton.on("click", function() {
+        modal.remove();
+      });
+      
+      leftButton.on("click", function () {
+        const prevCard = card.prev(".card");
+        if (prevCard.length > 0) {
+          modalContent.remove();
+          modalContent = prevCard.clone();
+          modalContainer.prepend(modalContent);
+          card = prevCard;
+        }
+      });
+      
+      rightButton.on("click", function () {
+        const nextCard = card.next(".card");
+        if (nextCard.length > 0) {
+          modalContent.remove();
+          modalContent = nextCard.clone();
+          modalContainer.prepend(modalContent);
+          card = nextCard;
+        }
+      });
+      
+      modal.show();
+    }
